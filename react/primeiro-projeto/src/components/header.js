68b5ef25b66fa6ec5aac0da1d690eb9e6a335ef4
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2022-08-12 21:35:23 -0300

    adicionando pasta de react

---
+++ react/primeiro-projeto/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/global.css'
+
+const Header = () => {
+    return (
+        <div className='centered'>
+            <h1 className=''>Titulo</h1>
+        </div>
+    );
+};
+
+export default Header;
