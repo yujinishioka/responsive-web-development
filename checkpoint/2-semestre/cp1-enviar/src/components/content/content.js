3ee6d2640c6ced32e9126c412793fe886d4a122f
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2022-08-25 22:01:23 -0300

    progredindo com checkpoint 3

---
+++ checkpoint/2-semestre/cp1-enviar/src/components/content/content.js
@@ -0,0 +1,18 @@
+import '../../css/App.css'
+
+const Content = () => {
+    return (
+        <section className="header-site">
+            <div className="container">
+                <h1 className="text-center">
+                    Seja bem-vindo ao drone parallax
+                </h1>
+                <p className="text-center lead">
+                    Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Non, atque maiores fugit iste ad pariatur adipisci eaque temporibus autem distinctio, dolorum perferendis, similique nesciunt facilis sunt voluptate, modi repellendus dolorem.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Content;
--- checkpoint/2-semestre/cp1-enviar/src/components/header/header.js
+++ checkpoint/2-semestre/cp1-enviar/src/components/header/header.js
@@ -1,12 +1,17 @@
+import '../../css/App.css'
+import './header.css'
+
 const Header = () => {
     return (
-        <div className="container">
-            <header>
-                <h1 class="text-center">Seja bem-vindo ao drone parallax</h1>
-                <p class="text-center lead">Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Non, atque maiores fugit iste ad pariatur adipisci eaque temporibus autem distinctio, dolorum perferendis, similique nesciunt facilis sunt voluptate, modi repellendus dolorem.</p>
-                <p class="text-center"><a href="#" class="btn btn-vermelho">Cadastre-se</a></p>
-            </header>
-        </div>
+        <section className="header-site">
+            <div className="container">
+                <header>
+                    <h1 class="text-center">Seja bem-vindo ao drone parallax</h1>
+                    <p class="text-center lead">Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Non, atque maiores fugit iste ad pariatur adipisci eaque temporibus autem distinctio, dolorum perferendis, similique nesciunt facilis sunt voluptate, modi repellendus dolorem.</p>
+                    <p class="text-center"><a href="#" class="btn btn-vermelho">Cadastre-se</a></p>
+                </header>
+            </div>
+        </section>
     )
 }
 
