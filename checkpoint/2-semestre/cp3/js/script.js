fc56b943e4c541d867b0322a0294da8dffb362b2
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2022-10-14 23:34:07 -0300

    adicionando cp 6

---
+++ checkpoint/2-semestre/cp3/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function btnMenu(){
+	menu = document.getElementsByClassName("menu");
+	menu[0].style.display = "block";
+}
+
+
+function btnFechar(){
+	menuFechar = document.getElementsByClassName("menu");
+	menuFechar[0].style.display = "none";
+}
