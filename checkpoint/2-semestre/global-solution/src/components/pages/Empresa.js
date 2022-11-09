a6151efe36576582763de76232466b340719ee59
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2022-11-08 21:58:09 -0300

    imprimindo empresas mockadas

--- checkpoint/2-semestre/global-solution/src/components/pages/Empresa.js
+++ checkpoint/2-semestre/global-solution/src/components/pages/Empresa.js
@@ -1,10 +1,30 @@
 import { Link } from 'react-router-dom';
-import { EmpresaStyle } from '../../style/pages/empresa.js'
+import { 
+    EmpresaStyle, 
+    Items, 
+    Field, 
+    } from '../../style/pages/empresa.js'
 
 const Empresa = () => {
+    let items = [[1, 'Sioux', 'teste'], [2, 'FIAP', 'teste']]
+
+    let itemList = items.map((item, index) => {
+        return <>
+            <Field key={index}>
+                <p>ID: {item[0]}</p>
+                <p>Nome: {item[1]}</p>
+                <p>Descricao: {item[2]}</p>
+            </Field>
+        </>
+    })
+
     return (
         <EmpresaStyle className='container'>
             <h2>Empresa</h2>
+
+            <Items>
+                {itemList}
+            </Items>
 
             <Link to="/main">
                 <button className='btn btn-voltar'>Voltar</button>
--- checkpoint/2-semestre/global-solution/src/style/pages/empresa.js
+++ checkpoint/2-semestre/global-solution/src/style/pages/empresa.js
@@ -7,6 +7,25 @@ export const EmpresaStyle = styled.div`
     text-align: center;
 
     h2 {
+        margin-top: 20px;
+        font-size: 32px;
+        text-transform: uppercase;
+        color: ${colors.primaryColor};
+    }
+`
+
+export const Items = styled.div`
+    width: 100%;
+    margin-top: 20px;
+`
+
+export const Field = styled.div`
+    margin: 10px auto;
+    padding: 8px 0;
+    border: 1px solid ${colors.primaryColor};
+    border-radius: 8px;
+
+    p {
         color: ${colors.primaryColor}
     }
 `
